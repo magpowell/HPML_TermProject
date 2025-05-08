8b86991d1581592c92a3b3adc3893e2289ce1c50
Jake Halpern <jmh2363@columbia.edu>
2025-05-08 08:46:12 -0700

    better figure parameters

--- plot_quantization.py
+++ plot_quantization.py
@@ -14,17 +14,19 @@ fig, axs = plt.subplots(3, 1, figsize=(10, 12), sharex=True)
 
 # Plot 1: Average Accuracy
 axs[0].bar(df['qdtype'], df['avg_rmse'], color='skyblue')
-axs[0].set_ylabel("Average RMSE")
-axs[0].set_title("Quantization Impact on Accuracy, Time, and Size")
+axs[0].set_ylabel("Avg RMSE", fontsize=18, fontweight='bold')
+axs[0].tick_params(axis='both', labelsize=14)
 
 # Plot 2: Average Time
 axs[1].bar(df['qdtype'], 1000*df['avg_time'], color='salmon')
-axs[1].set_ylabel("Average Inference Time (ms)")
+axs[1].set_ylabel("Avg Inference Time (ms)", fontsize=18, fontweight='bold')
+axs[1].tick_params(axis='both', labelsize=14)
 
 # Plot 3: Model Size Reduction
 axs[2].bar(df['qdtype'], df['quantized_model_size'], color='seagreen')
-axs[2].set_ylabel("Quantized Model Size (%)")
-axs[2].set_xlabel("Quantization Data Type")
+axs[2].set_ylabel("Quantized Model Size (%)", fontsize=18, fontweight='bold')
+axs[2].set_xlabel("Quantization Data Type", fontsize=18, fontweight='bold')
+axs[2].tick_params(axis='both', labelsize=14)
 
 plt.tight_layout()
 plt.savefig("quantization_results.png")
